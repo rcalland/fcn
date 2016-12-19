7883c9fa3530651d75ac5a3c6b5fed3b1fced31f
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-19 10:52:21 +0900

    Download mit dataset by script

--- examples/apc2016/download_dataset.py
+++ examples/apc2016/download_dataset.py
@@ -27,5 +27,20 @@ def main():
     fcn.util.extract_file(path, to_directory=dataset_dir)
 
 
+    dataset_dir = chainer.dataset.get_dataset_directory('apc2016mit')
+    path = osp.join(dataset_dir, 'training.tgz')
+    fcn.data.cached_download(
+        url='https://drive.google.com/uc?id=0B4mCa-2YGnp7ZEMwcW5rcVBpeG8',
+        path=path,
+        md5='14298453fcdbcaaa6b3fa8439a486e6d',
+    )
+    path = osp.join(dataset_dir, 'benchmark.tgz')
+    fcn.data.cached_download(
+        url='https://drive.google.com/uc?id=0B9P1L--7Wd2vOHhlbENra2REZDA'
+        path=path
+        md5='15a6ff714fafb3950c0b0ff0161d6ef6',
+    )
+
+
 if __name__ == '__main__':
     main()
