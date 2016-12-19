77544f7da48989b1b26af8282914afae167eb1ac
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-19 10:25:29 +0900

    Add option to train with APC2016MITDataset

--- examples/apc2016/train_fcn32s.py
+++ examples/apc2016/train_fcn32s.py
@@ -11,18 +11,23 @@ import datasets
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', default='jsk', choices=('jsk', 'mit'))
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('-o', '--out', default='logs/latest')
     parser.add_argument('--resume')
     args = parser.parse_args()
 
+    if args.dataset == 'jsk':
+        dataset_class = datasets.APC2016JSKDataset
+    else:
+        dataset_class = datasets.APC2016MITDataset
     gpu = args.gpu
     out = args.out
     resume = args.resume
     max_iter = 100000
 
     trainer = fcn.trainers.fcn32s.get_trainer(
-        dataset_class=datasets.APC2016JSKDataset,
+        dataset_class=dataset_class,
         gpu=gpu,
         max_iter=max_iter,
         out=out,
